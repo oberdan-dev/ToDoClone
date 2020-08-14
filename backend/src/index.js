b14b24a4b4da3e40e6ea11bbba3b350315de55e8
oberdan-dev <ocsantosdev@gmail.com>
2020-08-14 00:44:42 -0300

    ajustes no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes)
 
-server.listen(3000, () => {
+server.listen(3001, () => {
     console.log('API ONLINE');
 });
